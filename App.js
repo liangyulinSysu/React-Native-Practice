65e978b89fc0a79ce56892b07d885f21b5cab8bb
liangyulinSysu <liangyulinSysu@163.com>
2018-01-10 20:15:26 -0800

    navigator

--- App.js
+++ App.js
@@ -11,17 +11,29 @@ import {
   Text,
   View
 } from 'react-native';
+import {StackNavigator} from 'react-navigation';
 
 var Page1 = require("./View/Page1");
 var Page2 = require("./View/Page2");
 
-export default class App extends Component<{}> {
-  render(){
-    return(
-      <Page2 style={styles.container}/>
-    );
+export default App = StackNavigator(
+  {
+    Home: {screen : Page1},
+    Second: {screen : Page2}
+  },
+  {
+      // 配置初始化路由名称
+       initialRouteName: 'Second',
+       // 配置初始化路由的参数
+       initialRouteParams: {content: '初始化传入的参数'},
+       // 配置默认的 navigationOptions
+       navigationOptions: {headerRight: <Text>右边Header</Text>},
+       // 转场动画开始的回到
+       onTransitionStart: () => {
+           console.log('start')
+       },
   }
-}
+);
 
 const styles = StyleSheet.create({
   container: {
